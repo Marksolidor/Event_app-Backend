be70c39d750f0931b0b3184fc8f9484ca42afd02
Marksolidor <104401054+Marksolidor@users.noreply.github.com>
2023-04-01 18:39:54 -0300

    Update usersRoutes.js

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,55 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const Usuario = require('./../models/usersModel');
+const userController = require('../controllers/userController');
+const {isLogin} = require("../middlewares/isLogin")
 
-router.get('/', async (req, res) => {
-  try {
-    const usuario = await Usuario.obtenerUsuarios;
-    res.json(usuario);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// router.get("/", auth, async (req, res) => {
-//   try {
-//     await usuarioController.obtenerUsuariosAutentificado(req, res);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Hubo un error al obtener información del usuario");
-//   }
-// });
-
-router.get('/:id', async (req, res) => {
-  try {
-    res.json({ message: 'obtener usuarios por id' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    res.json({ message: 'Crear nuevo usuario' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.patch('/:id', async (req, res) => {
-  try {
-    res.json({ message: 'Actualizar usuario por id' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    res.json({ message: 'Eliminar usuario por id' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get('/', isLogin.userController.obtenerUsuariosAutentificado);
+router.get('/:id', userController.obtenerUsuarioPorId);
+router.post('/', userController.crearUsuario);
+router.patch('/:id', userController.actualizarUsuario);
+router.delete('/:id', userController.eliminarUsuario);
 
 module.exports = router;
+
